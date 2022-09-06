eaf2017f4a1d9b2bd7f2ac54f1d65754082de74b
Victor Emmanuel <victorpassos2012@gmail.com>
2022-09-06 02:51:11 -0300

    Requisito 1 feito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    students_count = 0
+    try:
+        for student in permanence_period:
+            start = student[0]
+            finish = student[1]
+            if start <= target_time <= finish:
+                students_count += 1
+        return students_count
+    except TypeError:
+        return None
